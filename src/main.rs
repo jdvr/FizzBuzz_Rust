c3585e5e15db06fc56ccfd3a5fc3f8fb2666151c
jdvr <juandavidvegarguez@gmail.com>
2018-05-18 01:46:32 +0200

    use enum and patter matching

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,34 @@
 // Both  -> FizzBuzz
 // _ -> number
 
+enum FizzBuzz {
+    Fizz,
+    Buzz,
+    FizzBuzz,
+    Other(u32),
+}
+
+fn process(number: u32) -> FizzBuzz {
+    match number % 3 {
+        0 => match number % 5 {
+            0 => FizzBuzz::FizzBuzz,
+            _ => FizzBuzz::Fizz,
+        },
+        _ => match number % 5 {
+            0 => FizzBuzz::Buzz,
+            _ => FizzBuzz::Other(number),
+        },
+    }
+}
+
 fn main() {
     for number in 1..20 {
-        if number % 3 == 0 && number % 5 == 0 {
-            println!("FizzBuzz");
-        } else if number % 3 == 0 {
-            println!("Fizz");
-        } else if number % 5 == 0 {
-            println!("Buzz");
-        } else {
-            println!("{}", number);
+        match process(number) {
+            FizzBuzz::Fizz => println!("Fizz"),
+            FizzBuzz::Buzz => println!("Buzz"),
+            FizzBuzz::FizzBuzz => println!("FizzBuzz"),
+            FizzBuzz::Other(n) => println!("{}", n),
         }
+
     }
 }
