e301744f52c0a0fd7c924eafba6b36ce226c5fa6
jdvr <juandavidvegarguez@gmail.com>
2018-05-18 01:51:09 +0200

    convert process in a fizzbuzz associated fn

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,24 @@ enum FizzBuzz {
     Other(u32),
 }
 
-fn process(number: u32) -> FizzBuzz {
-    match number % 3 {
-        0 => match number % 5 {
-            0 => FizzBuzz::FizzBuzz,
-            _ => FizzBuzz::Fizz,
-        },
-        _ => match number % 5 {
-            0 => FizzBuzz::Buzz,
-            _ => FizzBuzz::Other(number),
-        },
+impl FizzBuzz {
+    fn from(number: u32) -> FizzBuzz {
+        match number % 3 {
+            0 => match number % 5 {
+                0 => FizzBuzz::FizzBuzz,
+                _ => FizzBuzz::Fizz,
+            },
+            _ => match number % 5 {
+                0 => FizzBuzz::Buzz,
+                _ => FizzBuzz::Other(number),
+            },
+        }
     }
 }
 
 fn main() {
     for number in 1..20 {
-        match process(number) {
+        match FizzBuzz::from(number) {
             FizzBuzz::Fizz => println!("Fizz"),
             FizzBuzz::Buzz => println!("Buzz"),
             FizzBuzz::FizzBuzz => println!("FizzBuzz"),
